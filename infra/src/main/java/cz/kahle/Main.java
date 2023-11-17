5a6b02f0f8a00d8661f07b0fa1991aff39bf3eed
Pavel Kahle <PavelKa>
2023-11-17 16:25:27 +0000

    Initial commit

---
+++ infra/src/main/java/cz/kahle/Main.java
@@ -0,0 +1,11 @@
+package cz.kahle;
+
+import software.amazon.awscdk.App;
+
+public class Main {
+    public static void main(final String[] args) {
+        App app = new App();
+        new AppStack(app, "MicronautAppStack");
+        app.synth();
+    }
+}
